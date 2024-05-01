8196448e01dd05340aef5d1d7065cfa7c4238947
agenorfiuza <afiuzane@gmail.com>
2024-05-01 07:04:53 -0300

    tipos de operadores de comparação

---
+++ tipos_de_operadores_comparacao.py
@@ -0,0 +1,20 @@
+saldo = 200
+saque = 200
+
+# igualdade
+print (saldo == saque)
+
+# diferença
+print (saldo != saque)
+
+# maior que
+print (saldo > saque)
+
+# maior ou igual
+print (saldo >= saque)
+
+# menor que
+print (saldo < saque)
+
+# menor ou igual
+print (saldo <= saque)
