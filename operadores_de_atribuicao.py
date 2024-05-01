554de76885c9e3a168a018a3784281f647f6fd58
agenorfiuza <afiuzane@gmail.com>
2024-05-01 07:35:09 -0300

    operadores de atribuição

---
+++ operadores_de_atribuicao.py
@@ -0,0 +1,41 @@
+# atribuição simples
+saldo = 500
+print (saldo)
+#>>> 500
+
+# atribuição com adição
+saldo += 200
+print (saldo)
+#>>> 700
+
+# atribuição com subtração
+saldo -= 200
+print (saldo)
+#>>> 500
+
+# atribuição com multiplicação
+saldo *= 2
+print (saldo)
+#>>> 1000
+
+# atribuição com divisão
+saldo /= 5
+print (saldo)
+#>>> 200.0
+
+# atribuição com divisão inteira
+saldo = 200
+saldo //= 2
+print (saldo)
+#>>> 100
+
+# atribuição por modulo resto da divisão
+saldo %= 80
+print (saldo)
+#>>> 20
+
+# atribuição com exponenciação
+saldo = 80
+saldo **= 2
+print (saldo)
+#>>> 6400
